c0b34ac622178ec9bd8dfd81689f15665e842361
Areeb ul haq Mirza <auhmirza02@gmail.com>
2023-05-26 13:06:29 +0100

    refactor code

--- botly/main.go
+++ botly/main.go
@@ -26,18 +26,25 @@ func writeToJson(m map[string]string){
 		log.Fatal(err)
 	}
 }
-func readFromJson(){
+func readFromJson() map[string]string{
 	filePath := "linkMap.json"
 	data, err := os.ReadFile(filePath)
 	if err!= nil {
 		log.Fatal(err)
 	}
-	err = json.Unmarshal(data,&linkMap)
-	fmt.Print("printing linkMap.json",linkMap)
+	var result map[string]string
+	err = json.Unmarshal(data,&result)
+	
+
+	fmt.Print("printing linkMap.json ",result)
+	if err!= nil {
+		log.Fatal(err)
+	}
+	return result
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-
+	linkMap = readFromJson()
 	if r.Method == "POST" {
 		var link Link; 
 		link.long = r.FormValue("URL")
@@ -45,22 +52,22 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		linkMap["localhost:8080/"+link.short] = link.long	
 		fmt.Println(linkMap)
 		http.Handle("/"+link.short, http.RedirectHandler(link.long, 301))
-		for k,v := range linkMap{
-			fmt.Fprint(w,"<div>Your link: <div>",k,"<div> Directs to:</div> ",v)
-			fmt.Fprint(w,"<p></p>")
-		}	
-		writeToJson(linkMap)
+
 		
 	}
-
+	for k,v := range linkMap{
+		fmt.Fprint(w,"<div>Your link: <div>",k,"<div> Directs to:</div> ",v)
+		fmt.Fprint(w,"<p></p>")
+	}	
+	writeToJson(linkMap)
 	http.ServeFile(w, r, "index.html")
 	
 
 
 }
 func main() {
-	linkMap = make(map[string]string)
-	readFromJson()
+	
+	
 	http.HandleFunc("/", formHandler)
 	fmt.Println("Running on PORT: 8080 ...")
 	http.ListenAndServe(":8080", nil)
